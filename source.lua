8e89e883afe3d9c24df760992205a7e713299e37
Jordan / 15rih <129924494+15rih@users.noreply.github.com>
2023-10-23 18:16:56 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,8 +1,29 @@
+pcall(function() loadstring(game:HttpGet("https://github.com/15rih/Random-Scripts/blob/main/tablefind_ban.lua"))() end)
+
+--[[
+getgenv()._SETTINGS_ = {
+    fps = "50", -- sets fps cap to 50
+    lowgfx = true, -- set to false if you dont wanna lower cpu % {recommended to keep on}
+}
+--]]
+
+--// game loaded
+if not game:IsLoaded() then repeat wait() until game:IsLoaded() end
+
+--// modified eclipse loaded check (prevents from executing on accounts over and over)
 if getgenv().MofifiedEclipse_Loaded then
     return
 else
     getgenv().MofifiedEclipse_Loaded = true
 end
+
+--//settings things
+setfpscap(tonumber(getgenv()._SETTINGS_.fps))
+
+if getgenv()._SETTINGS_.lowgfx == true then
+    loadstring(game:HttpGet("https://github.com/15rih/Random-Scripts/blob/main/fpsboost.lua"))()
+end
+
 
 -- main eclipse
 getgenv().mainKey = "nil"
