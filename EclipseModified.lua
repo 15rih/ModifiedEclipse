ab936a03368ace38b46898ee984c30f07e2bbc46
Jordan / 15rih <129924494+15rih@users.noreply.github.com>
2023-10-23 18:17:25 -0700

    Update EclipseModified.lua

--- EclipseModified.lua
+++ EclipseModified.lua
@@ -1,11 +1,5 @@
-if getgenv().MofifiedEclipse_Loaded then
-    return
-else
-    getgenv().MofifiedEclipse_Loaded = true
-end
-
-
--- main eclipse
-getgenv().mainKey = "nil"
-
-local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+getgenv()._SETTINGS_ = {
+    fps = "50", -- sets fps cap to 50
+    lowgfx = true, -- set to false if you dont wanna lower cpu % {recommended to keep on}
+}
+loadstring(game:HttpGet("https://github.com/15rih/ModifiedEclipse/blob/main/source.lua"))()
